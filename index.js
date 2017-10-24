e49fe03f57533a84edfc452bb46939afe7861184
ThomasSzymanski <thomas.szymanski15@gmail.com>
2017-10-24 14:31:44 +0200

    Exercice 8 - Partie 1 - JAVASCRIPT

--- index.js
+++
@@ -1,6 +0,0 @@
-function diviser(){
-	var first = document.getElementById('premier_nombre').value;
-    var second = document.getElementById('deuxieme_nombre').value;
-    var division = first % second;
-    alert(division);
-}
